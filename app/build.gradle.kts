b66ab576de8c46c4bb0dcd5b864ce653c8f6e135
naveeneppalapally <naveeneppalapally@gmail.com>
2026-04-06 12:09:48 +0530

    Bump version to 1.3.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.naveen.aerialviewsplus"
         minSdk = 23 // Android v6
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.3.14"
+        versionCode = 22
+        versionName = "1.3.15"
         betaVersion = "-beta12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
